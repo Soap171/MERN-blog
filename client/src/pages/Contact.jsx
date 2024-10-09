5547223887538577e9742e2db81ddabde98198f4
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-10-09 20:43:52 +0530

    add contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ContactHeader from "../components/ContactHeader";
+import SocialLine from "../components/SocialLine";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <ContactHeader />
+      <SocialLine />
+    </div>
+  );
 }
 
 export default Contact;
